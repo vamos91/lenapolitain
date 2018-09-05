c90cebaa42cfbd8f582343a8d041bfa0b166cf6e
nicolas <acardnicolas@hotmail.com>
2018-09-05 14:24:41 +0200

    update

--- app/controllers/croisieres_controller.rb
+++ app/controllers/croisieres_controller.rb
@@ -23,6 +23,10 @@ class CroisieresController < ApplicationController
     @reviews = Review.where(croisiere_id: @croisiere.id).reverse
     @review = Review.new
     @croisieres_par_destination = Croisiere.where(croisiere_destination: @croisiere.croisiere_destination)
+    sum_reviews = 0
+    @reviews.each {|num| sum_reviews += num.rating.to_i}
+    @average_rating = sum_reviews/@reviews.count
+
   end
 
   def update
