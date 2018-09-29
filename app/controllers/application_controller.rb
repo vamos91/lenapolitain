686eeb3760f6afcec382e7cbe2e863e6668e8f0f
nicolas <acardnicolas@hotmail.com>
2018-09-29 14:20:04 +0200

    update home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,6 @@ class ApplicationController < ActionController::Base
     @croisieres_napolitain = Croisiere.all.sort_by {|a| a.croisiere_destination}
   end
 
-
   def default_url_options
   { host: ENV["HOST"] || "localhost:3000" }
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   def home
     croisieres_all = Croisiere.all
     @croisieres = croisieres_all.sort_by {|a| a.croisiere_destination}
+   # @last_reviews = Review.all.last(3)
+   @last_review = Review.last
   end
+
 end
